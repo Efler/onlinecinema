a956345ff90d7e17c7f918c62e5e51e76a80b1a7
yungalaxxxy <alex.abrosimov2@yandex.ru>
2022-07-05 18:55:11 +0300

    CHANGES: new location for mainpage

--- frontend/cinema-front/src/App.js
+++ frontend/cinema-front/src/App.js
@@ -1,41 +1,19 @@
 import './App.css';
 
 import React, {Component} from "react";
-import MainPageComponent from "./components/MainPage";
-import axios from 'axios'
+import MainPageComponent from "./pages/MainPage";
+import Player from "./components/Player";
 
-export function create_pic_url(id){
-    return "http://0.0.0.0:8000/get_image/"+id
+
+export function create_pic_url(id) {
+    return "http://0.0.0.0:8000/get_image/" + id
 }
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            films: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get('http://0.0.0.0:8000')
-            .then(response => {
-                this.setState({
-                    films: response.data.films
-                })
-                console.log(response.data.films)
-            })
-
-    }
-
     render() {
-        const {films} = this.state
         return (
             <div>
-                <h1>Cinema bro</h1>
-                {films.map(film => <div>
-                    <p>{film.id} {film.name} - {film.description}</p>
-                    <img src={create_pic_url(film.id)} width="189" height="255" alt="poster"/>
-                </div>)}
+                <MainPageComponent/>
             </div>
 
         );
---
+++ frontend/cinema-front/src/components/Player.js
@@ -0,0 +1,11 @@
+import ReactPlayer from 'react-player'
+
+const Video = (word) => {
+    return (
+      <div>
+          <h1> {word}</h1>
+      </div>
+    );
+}
+
+export default Video
